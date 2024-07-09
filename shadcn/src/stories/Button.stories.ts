fe8cf859bc9da5f33f8d11671c66f06637a348a0
TransonQ <quanscheng@qq.com>
2024-07-09 16:01:25 +0800

    deps: dnd

--- shadcn/src/stories/Button.stories.ts
+++ shadcn/src/stories/Button.stories.ts
@@ -9,6 +9,9 @@ const meta: Meta<typeof TestButton> = {
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
+    backgrounds: {
+      // default: 'dark',
+    },
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
