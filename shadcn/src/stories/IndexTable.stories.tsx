a25942616b745ecf524c313e5356eca7e420813d
TransonQ <quanscheng@qq.com>
2024-07-10 16:29:06 +0800

    docs

--- shadcn/src/stories/IndexTable.stories.tsx
+++ shadcn/src/stories/IndexTable.stories.tsx
@@ -4,7 +4,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof IndexTable> = {
-  title: "shared/IndexTable",
+  title: "composite/IndexTable",
   component: ExampleTable,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
@@ -13,7 +13,7 @@ const meta: Meta<typeof IndexTable> = {
     },
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: [""],
+  tags: [],
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
 };
 
