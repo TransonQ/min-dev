6d118d38b1dc0c5d2d8ee21ef79d3bbef1fbd067
TransonQ <quanscheng@qq.com>
2024-07-11 14:33:32 +0800

    完善 showdocs

--- shadcn/src/utils/ShowDocs.tsx
+++ shadcn/src/utils/ShowDocs.tsx
@@ -1,17 +1,29 @@
 import { MDXProvider } from "@mdx-js/react";
 import React from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { defaultStyle as dark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+// https://mdxjs.com/guides/syntax-highlighting/
+function code({ className, ...properties }: any) {
+  const match = /language-(\w+)/.exec(className || "");
+  return match ? (
+    <SyntaxHighlighter
+      language={match[1]}
+      PreTag="div"
+      {...properties}
+      style={dark}
+    />
+  ) : (
+    <code className={className} {...properties} />
+  );
+}
 
 // 这个组件接受一个 MDX 文件作为参数并渲染它
 const MDXRenderer: React.FC<{ mdxComponent: React.ComponentType }> = ({
   mdxComponent: MDXComponent,
 }) => {
   return (
-    <MDXProvider
-      components={{
-        // 用 CodeBlock 组件替换 MDX 中的代码块
-        // code: CodeBlock,
-      }}
-    >
+    <MDXProvider components={{ code }}>
       <MDXComponent />
     </MDXProvider>
   );
